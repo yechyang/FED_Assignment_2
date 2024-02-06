893b724e2d15aad33b20e481073c4c0903ffd282
kohye <kohyechyang11@gmail.com>
2024-02-06 21:18:40 +0800

    done with snake game and the js

--- js/lottie.js
+++ js/lottie.js
@@ -10,7 +10,7 @@ function hideLoader() {
 }
 
 // Hide the loader after a certain period of time (time taken to load was too fast to see animation) (e.g., 3000 milliseconds = 3 seconds)
-setTimeout(hideLoader, 5000);
+setTimeout(hideLoader, 4000);
 
 // Event listener to hide the loader when the page has fully loaded
 //window.addEventListener("load", function(){
